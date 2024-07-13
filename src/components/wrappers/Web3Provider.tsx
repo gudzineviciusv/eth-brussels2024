61d84dfe3b05558879ee1d4c27b562fc8a462b5b
gudzineviciusv <vytautas+1@bitdegree.org>
2024-07-13 22:05:25 +0200

    Fix - fixed supported chains

--- src/components/wrappers/Web3Provider.tsx
+++ src/components/wrappers/Web3Provider.tsx
@@ -4,12 +4,37 @@ import React from 'react';
 import { WagmiProvider, createConfig } from 'wagmi';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ConnectKitProvider, getDefaultConfig } from 'connectkit';
-import { sepolia, linea } from 'wagmi/chains';
+import { sepolia, linea, arbitrumNova, arbitrumSepolia, lineaTestnet , zircuitTestnet} from 'wagmi/chains';
+import { defineChain } from 'viem';
+
+
+export const incoNetwork = /*#__PURE__*/ defineChain({
+  id: 9090,
+  name: 'Inco Gentry Testnet',
+  nativeCurrency: {
+    decimals: 18,
+    name: 'INCO',
+    symbol: 'INCO',
+  },
+  rpcUrls: {
+    default: {
+      http: ['https://testnet.inco.org'], // Replace with actual RPC URL
+    },
+  },
+  blockExplorers: {
+    default: {
+      name: 'Mainnet Explorer',
+      url: 'https://explorer.testnet.inco.org', // Replace with actual block explorer URL
+    },
+  },
+});
+
 
 const config = createConfig(
     // @ts-ignore
   getDefaultConfig({
     appName: 'ComeToMyFuneral',
+    chains: [ lineaTestnet, arbitrumNova, zircuitTestnet, incoNetwork],
   })
 );
 
@@ -19,7 +44,7 @@ export const Web3Provider = ({ children }: { children: React.ReactNode }) => {
   return (
     <WagmiProvider config={config}>
       <QueryClientProvider client={queryClient}>
-        <ConnectKitProvider debugMode>{children}</ConnectKitProvider>
+        <ConnectKitProvider >{children}</ConnectKitProvider>
       </QueryClientProvider>
     </WagmiProvider>
   );
