a66f22b0c6d0c0a90dc72cdb5df8f41fa650b998
admin <admin@admins-MacBook-Air.local>
2024-07-13 09:43:47 -0700

    main page

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,19 +2,13 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="bg-black py-4 w-full">
-      <div className="container mx-auto flex items-center justify-between px-8">
-        <div className="text-white font-bold text-2xl">
-          NFT <span className="text-purple-400">SEA</span>
-        </div>
-        <div className="text-gray-400">
-          NFT Sea 2022 © All rights reserved
-        </div>
-        <button className="bg-gradient-to-r from-blue-500 to-pink-500 text-white px-4 py-2 rounded-lg">
-          Explore Marketplace
-        </button>
-      </div>
-    </footer>
+      <footer className="bg-black py-4 w-full">
+          <div className="container mx-auto flex items-center justify-center px-8">
+              <div className="text-gray-400 text-center">
+                  <p>Funeral ETH Brussel © All rights reserve</p>
+              </div>
+          </div>
+      </footer>
   );
 };
 
--- src/components/wrappers/BackgroundWrapper.tsx
+++ src/components/wrappers/BackgroundWrapper.tsx
@@ -2,9 +2,16 @@ import React from 'react';
 
 const BackgroundComponent: React.FC<React.PropsWithChildren<{}>> = ({ children }) => {
   return (
-    <div className="min-h-screen flex-col bg-gradient-to-r from-purple-800 via-purple-900 to-black flex items-center justify-between">
-      {children}
-    </div>
+      <div
+          className="min-h-screen flex flex-col items-center justify-between"
+          style={{
+            backgroundImage: `url('/background.png')`,
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+          }}
+      >
+        {children}
+      </div>
   );
 };
 
