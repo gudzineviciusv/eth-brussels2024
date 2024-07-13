cc103577fa4d3685011cd8a2ece7264a04606aa9
admin <admin@admins-MacBook-Air.local>
2024-07-13 11:57:43 -0700

    comment connect button

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,29 +1,27 @@
 import React, { useState } from 'react';
 import Modal from './BlockScoutModal'; // Adjust the path as necessary
+import { ConnectKitButton } from 'connectkit';
 
 const TopBar = () => {
-  const [isModalOpen, setIsModalOpen] = useState(false);
+    const [isModalOpen, setIsModalOpen] = useState(false);
 
-  const openModal = () => setIsModalOpen(true);
-  const closeModal = () => setIsModalOpen(false);
+    const openModal = () => setIsModalOpen(true);
+    const closeModal = () => setIsModalOpen(false);
 
-  return (
-    <div className="flex items-center justify-between px-8 w-full">
-      <div className="text-white font-bold text-2xl">
-        NFT <span className="text-purple-400">SEA</span>
-      </div>
-      <button
-        className="bg-gradient-to-r from-blue-500 to-pink-500 text-white px-4 py-2 rounded-lg"
-        onClick={openModal}
-      >
-        Check Wallet
-      </button>
-      <button className="bg-gradient-to-r from-blue-500 to-pink-500 text-white px-4 py-2 rounded-lg">
-        Explore Marketplace
-      </button>
-      {isModalOpen && <Modal isOpen={isModalOpen} onClose={closeModal} />}
-    </div>
-  );
+    return (
+        <div className="flex items-center justify-between px-8 w-full">
+            <button
+                className="bg-gradient-to-r from-blue-500 to-pink-500 text-white px-4 py-2 rounded-lg"
+                onClick={openModal}
+            >
+                Check Wallet
+            </button>
+            {isModalOpen && <Modal isOpen={isModalOpen} onClose={closeModal}/>}
+            {/*<div className="flex justify-end items-start px-8 py-4 w-full fixed top-0 right-0">*/}
+            {/*    <ConnectKitButton/>*/}
+            {/*</div>*/}
+        </div>
+    );
 };
 
 export default TopBar;
