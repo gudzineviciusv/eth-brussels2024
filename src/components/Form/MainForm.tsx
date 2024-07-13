4a2844a6c16166de82bc23a877c6ca132ee21b84
gudzineviciusv <vytautas+1@bitdegree.org>
2024-07-13 23:27:38 +0200

    Feat - added administration

--- src/components/Form/MainForm.tsx
+++ src/components/Form/MainForm.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import React, { useState, ChangeEvent } from 'react';
 import { ethers } from 'ethers';
@@ -13,19 +13,23 @@ import DescriptionInput from './Inputs/DescriptionalInput';
 import WhitelistInput from "@/components/Form/Inputs/WhitelistInput";
 import BlacklistInput from "@/components/Form/Inputs/BlacklistInput";
 import FuneralAdminInput from "@/components/Form/Inputs/FuneralAdminInput";
+import useAccountManager from '@/hooks/useFuneral';
 
 const MintNFTForm: React.FC = () => {
   const [selectedImage, setSelectedImage] = useState<File | null>(null);
   const [title, setTitle] = useState<string>('');
   const [description, setDescription] = useState<string>('');
-  const [metadata, setMetadataURI] = useState<string>('');
+  const [metadataURI, setMetadataURI] = useState<string>('');
   const [funeralAdminPublicAddress, setFuneralAdminPublicAddress] = useState<string>('');
   const [whitelistAddresses, setWhitelistAddresses] = useState<string>('');
   const [blacklistAddresses, setBlacklistAddresses] = useState<string>('');
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
   const [transactionHash, setTransactionHash] = useState<string>('');
+  const [currentStep, setCurrentStep] = useState<number>(1);
+
   const { mintNFT, isMinting, mintError } = useMintNFT(contractAddress, abi.abi);
   const { uploadToPinata, isUploading, uploadError } = usePinataUpload();
+  const { addToWhiteList, addToBlackList, setAdministratorAccount } = useAccountManager();
 
   const handleImageChange = (event: ChangeEvent<HTMLInputElement>) => {
     if (event.target.files && event.target.files[0]) {
@@ -34,7 +38,7 @@ const MintNFTForm: React.FC = () => {
   };
 
   const handleUpload = async () => {
-    if (!selectedImage) return;
+    if (!selectedImage || !title || !description) return;
 
     try {
       const uploadResult = await uploadToPinata(selectedImage, title, description);
@@ -43,70 +47,129 @@ const MintNFTForm: React.FC = () => {
         throw new Error('Failed to upload to Pinata');
       }
 
-      const { metadataURI } = uploadResult;
-      setMetadataURI(metadataURI);
-      setIsModalOpen(true);
+      setMetadataURI(uploadResult.metadataURI);
+      setCurrentStep(2);
     } catch (error) {
       console.error('Error uploading to Pinata:', error);
     }
   };
 
+  const handleNextStep = async () => {
+    try {
+      const provider = new ethers.providers.Web3Provider((window as any).ethereum);
+      await provider.send('eth_requestAccounts', []);
+
+      if (currentStep === 2) {
+        await addToWhiteList(whitelistAddresses, '');
+      } else if (currentStep === 3) {
+        await addToBlackList(blacklistAddresses, '');
+      } else if (currentStep === 4) {
+        await setAdministratorAccount(funeralAdminPublicAddress);
+      }
+
+      setCurrentStep(currentStep + 1);
+    } catch (error) {
+      console.error('Error performing contract action:', error);
+    }
+  };
+
   const handleMint = async () => {
-    if (!selectedImage) return;
+    if (!metadataURI) return;
 
     try {
       const provider = new ethers.providers.Web3Provider((window as any).ethereum);
       await provider.send('eth_requestAccounts', []);
       const signer = provider.getSigner();
 
-      const hash = await mintNFT(metadata, signer);
+      const hash = await mintNFT(metadataURI, signer);
       setTransactionHash(hash);
       setIsModalOpen(true);
     } catch (error) {
-      console.error('Error connecting wallet:', error);
+      console.error('Error minting NFT:', error);
     }
   };
 
   return (
-      <div className="max-w-lg mx-auto p-6 bg-gray-900 rounded-lg shadow-lg bg-opacity-85">
-
+    <div className="max-w-lg mx-auto p-6 bg-gray-900 rounded-lg shadow-lg bg-opacity-85">
+      {currentStep === 1 && (
+        <>
           <ImageUpload selectedImage={selectedImage} onImageChange={handleImageChange}/>
-          {uploadError && <p className="text-red-500">{uploadError}</p>}
-          {mintError && <p className="text-red-500">{mintError}</p>}
-          <div className="mb-6 text-center">
-              <h2 className="text-lg font-semibold text-gray-300">You Funeral NFT Information</h2>
-          </div>
           <TitleInput title={title} onTitleChange={(e) => setTitle(e.target.value)}/>
           <DescriptionInput description={description} onDescriptionChange={(e) => setDescription(e.target.value)}/>
-          <FuneralAdminInput publicAddress={funeralAdminPublicAddress}
-                             onPublicAddressChange={(e) => setFuneralAdminPublicAddress(e.target.value)}/>
-          <WhitelistInput whitelist={whitelistAddresses}
-                          onWhitelistChange={(e) => setWhitelistAddresses(e.target.value)}/>
-          <BlacklistInput blacklist={blacklistAddresses}
-                          onBlackListChange={(e) => setBlacklistAddresses(e.target.value)}/>
-
+          {uploadError && <p className="text-red-500">{uploadError}</p>}
           <div className="flex justify-between space-x-4">
-              <button
-                  className="px-4 py-2 bg-gray-700 bg-opacity-50 text-white rounded-lg focus:outline-none focus:ring-2 focus:ring-purple-600"
-                  onClick={handleUpload}
-                  disabled={isUploading || isMinting}
-              >
-                  {isUploading || isMinting ? <Loader/> : 'Create your death wish'}
-              </button>
+            <button
+              className="px-4 py-2 bg-gray-700 bg-opacity-50 text-white rounded-lg focus:outline-none focus:ring-2 focus:ring-purple-600"
+              onClick={handleUpload}
+              disabled={isUploading}
+            >
+              {isUploading ? <Loader/> : 'Upload Image and Generate Metadata'}
+            </button>
           </div>
+        </>
+      )}
 
-          <Modal
-              isOpen={isModalOpen}
-              onClose={() => setIsModalOpen(false)}
-              title={title}
-              description={description}
-              imageUrl={selectedImage ? URL.createObjectURL(selectedImage) : ''}
-              actionText="Continue"
-              onAction={handleMint}
-              isMinting={isMinting}
-              transactionHash={transactionHash}
-          />
-      </div>
+      {currentStep === 2 && (
+        <>
+          <WhitelistInput whitelist={whitelistAddresses} onWhitelistChange={(e) => setWhitelistAddresses(e.target.value)}/>
+          <button
+            className="px-4 py-2 bg-gray-700 bg-opacity-50 text-white rounded-lg focus:outline-none focus:ring-2 focus:ring-purple-600"
+            onClick={handleNextStep}
+          >
+            Next
+          </button>
+        </>
+      )}
+
+      {currentStep === 3 && (
+        <>
+          <BlacklistInput blacklist={blacklistAddresses} onBlackListChange={(e) => setBlacklistAddresses(e.target.value)}/>
+          <button
+            className="px-4 py-2 bg-gray-700 bg-opacity-50 text-white rounded-lg focus:outline-none focus:ring-2 focus:ring-purple-600"
+            onClick={handleNextStep}
+          >
+            Next
+          </button>
+        </>
+      )}
+
+      {currentStep === 4 && (
+        <>
+          <FuneralAdminInput publicAddress={funeralAdminPublicAddress} onPublicAddressChange={(e) => setFuneralAdminPublicAddress(e.target.value)}/>
+          <button
+            className="px-4 py-2 bg-gray-700 bg-opacity-50 text-white rounded-lg focus:outline-none focus:ring-2 focus:ring-purple-600"
+            onClick={handleNextStep}
+          >
+            Next
+          </button>
+        </>
+      )}
+
+      {currentStep === 5 && (
+        <>
+          <button
+            className="px-4 py-2 bg-gray-700 bg-opacity-50 text-white rounded-lg focus:outline-none focus:ring-2 focus:ring-purple-600"
+            onClick={handleMint}
+            disabled={isMinting}
+          >
+            {isMinting ? <Loader/> : 'Mint NFT'}
+          </button>
+        </>
+      )}
+
+      {mintError && <p className="text-red-500">{mintError}</p>}
+      <Modal
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+        title={title}
+        description={description}
+        imageUrl={selectedImage ? URL.createObjectURL(selectedImage) : ''}
+        actionText="Close"
+        onAction={() => setIsModalOpen(false)}
+        isMinting={isMinting}
+        transactionHash={transactionHash}
+      />
+    </div>
   );
 };
 
